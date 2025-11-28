92e0d70753adbb6c825c7f88c74e02017917e9f5
Barış Parlak <barisparlak16@gmail.com>
2025-11-28 20:57:48 +0300

    main page card created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import MainPage from './pages/MainPage.jsx'
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+
 import './App.css'
 import CourseDetailInfoCard from './components/courseDetailInfoCard'
+function HomePage() {
+  const navigate = useNavigate();
+    <div>
+      <button onClick={() => navigate("/home")} className="m-3">
+        Go to home
+      </button>
 
+    </div>
+}
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <CourseDetailInfoCard/>
+    <Router>
+<Routes>
+          <Route path="/" element={<MainPage/>} />
+
+        <Route path="/home" element={<HomePage/>} />
+
+</Routes>
+    </Router>
+
     </>
   )
 }
