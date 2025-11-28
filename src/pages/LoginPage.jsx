9e397716f384b7e3d4104bba4aa60b74739459ba
mkazancioglu <kazancioglumuratt@icloud.com>
2025-11-29 01:56:32 +0300

    loginAuth completed

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,14 +3,36 @@ import Navbar from '../components/Navbar.jsx';
 import '../css/authPage.css';
 import CustomButton from '../components/CustomButton.jsx';
 import {useNavigate} from 'react-router-dom';
-
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../config/firebase.js';
+import { useState } from 'react';
 function LoginPage() {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
     const navigate = useNavigate();
     const goToRegister = () => {
         navigate('/register');
     };
+    const handleLogin = async() =>  {
+        if(email=="" || password==""){
+          alert("Lütfen tüm alanları doldurunuz.");
+          return;
+        }
+        try{
+          await signInWithEmailAndPassword(auth,email,password);
+          alert("Giriş Başarılı");
+          navigate("/");
+        }catch(error){
+          if(error instanceof Error){
+            alert(error.message);
+          } else{
+            alert("Giriş sırasında bir hata oluştu.");
+          }
+        }
+      }
+
     return (
-        <div className="registerPage">
+        <div className="loginPage">
             <Navbar  />
                 <div className="row mt-5">
         <div className="col-4 ">
@@ -21,12 +43,12 @@ function LoginPage() {
 
                     <div className="d-flex flex-column align-items-start">
                     <h5 className="m-0 mx-2">E-Posta </h5> 
-                    <input type="email" placeholder="kaanbese@gmail.com" className="authInput form-control w-100 mb-3" />
+                    <input id="email" value={email} onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="kaanbese@gmail.com" className="authInput form-control w-100 mb-3" />
                     </div>
 
                     <div className="d-flex flex-column align-items-start">
                     <h5 className="m-0 mx-2">Şifre </h5> 
-                    <input type="password" placeholder="•••••••••••" className="authInput form-control w-100 mb-3" />
+                    <input id="password" value={password} onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="•••••••••••" className="authInput form-control w-100 mb-3" />
                     </div>
 
                     
@@ -39,9 +61,9 @@ function LoginPage() {
 
             </div>
 
-            <div class="d-flex justify-content-between align-items-start mt-3" >
+            <div className="d-flex justify-content-between align-items-start mt-3" >
                 <p className='btn btn-link' onClick={goToRegister} > Hesabınız yok mu, kayıt olun. </p>
-                <CustomButton text="Giriş Yap" />
+                <CustomButton myMethod={handleLogin} text="Giriş Yap" />
                 </div>            
 
 
