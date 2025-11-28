5e82852b9514774565976c066888e12c1d144e2f
Barış Parlak <barisparlak16@gmail.com>
2025-11-28 22:26:27 +0300

    main page
    main page frontend completed

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import '../css/mainPage.css';
 import MainCardComponent from '../components/MainCardComponent.jsx';
 import Navbar from '../components/Navbar.jsx';
+import {IoMdSearch , IoMdCodeWorking} from "react-icons/io";
+import { LuPenTool } from "react-icons/lu";
+import { BiMath } from "react-icons/bi";
+import { FaPaintBrush,FaGuitar } from "react-icons/fa";
 
 function MainPage() {
   return (
@@ -10,14 +14,14 @@ function MainPage() {
       <div className="search-container">
         <div className="search-box">
           <input type="text" placeholder="Ne öğrenmek istersin?" className="search-input" />
-          <span className="search-icon"> ICON </span>
+          <span className="search-icon"> <IoMdSearch size={25} /> </span>
         </div>
         <div className="filter-buttons">
-          <button className="filter-btn"> Kodlama</button>
-          <button className="filter-btn"> Tasarım</button>
-          <button className="filter-btn">Matematik</button>
-          <button className="filter-btn"> Resim</button>
-          <button className="filter-btn"> Enstrüman</button>
+          <button className="filter-btn">  <IoMdCodeWorking size={25} /> Kodlama</button>
+          <button className="filter-btn"><LuPenTool size={25} /> Tasarım</button>
+          <button className="filter-btn"><BiMath size={25} />Matematik</button>
+          <button className="filter-btn"><FaPaintBrush size={25} /> Resim</button>
+          <button className="filter-btn"> <FaGuitar size={25} />Enstrüman</button>
         </div>
       </div>
       <div className="title-section">
